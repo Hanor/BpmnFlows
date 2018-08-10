7395539f27ba0520c43317adc6fbec94ba921354
Hanor Sátiro Cintra <hanor_13ctb@hotmail.com>
2018-08-10 15:37:26 -0300

    [ISSUE-1] Was updated some packages version to fix vulnerability

--- bpmn-flows-demo/src/app/app.component.ts
+++ bpmn-flows-demo/src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'bpmn-flows-demo';
-  fileUrl = '../assets/implantacao.bpmn';
+  title = 'FlowsBpmn';
+  fileUrl = 'assets/implantacao.bpmn'
 }
